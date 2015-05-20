47b7cc4c1035c7841a976ea817bfb1ac1ed89702
Wryda <kakanuj@gmail.com>
2015-05-20 20:07:41 +0300

    Found a new solution for problem 5.
    No code required. Just multiplication of factors.

--- Problem5.py
+++ Problem5.py
@@ -1,17 +1,21 @@
-my_list = range(1,21)
-global number_count = 0
-global list_check = 0
+"""my_list = range(1,21)
+list_check = 0
 def number_iterator():
+    number_count = 0
     while list_check < 20:
-        
         number_checker()
         number_count += 1
     if list_check == 20:
         print number_count
 def number_checker():
+    number_count = 0
     for i in my_list:
         list_check = 0
         if number_count % i == 0:
             list_check += 1
 number_iterator()
-#FinishTmrw. 
+"""
+#So I found that there was an easier way to solve this problem that didn't require code at all. You already know the number that all numbers 
+#1 - 10 evenly divide into(2520). Then you have to find the product of all primes from 11-20 and multiply it with 2520 and 2. 
+# You need 2 because 2 also factors into the numbers twice. 20 -> 5*4 & 10* 2. 
+#The answer comes out to 232792560. Hope this helped!
